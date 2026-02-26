03c88d3a98ea6b4fb4d02d3580d9652898723bd5
Soumik Sardar <soumiksardar000@gmail.com>
2026-02-26 11:53:08 +0530

    media1

--- hackathon_main/settings.py
+++ hackathon_main/settings.py
@@ -132,12 +132,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    'hackathon_main/static',
-] 
+    BASE_DIR / 'hackathon_main' / 'static',
+]
 
+# White Noise compression for efficient static file serving
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
